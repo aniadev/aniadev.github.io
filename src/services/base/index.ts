a05587dfd91d9e509abd488d0fca66576ddd40ad
Hai Pham <haipc@beedu.vn>
2023-03-10 00:33:47 +0700

    clear cache

--- src/services/base/index.ts
+++
@@ -1,9 +0,0 @@
-import { BaseRepositoryInterface } from '@/interfaces/services/services.interface'
-
-export abstract class BaseRepository implements BaseRepositoryInterface {
-  prefix: string
-
-  constructor(prefix: string) {
-    this.prefix = prefix
-  }
-}
--- src/services/repositories/auth/index.ts
+++
@@ -1,8 +0,0 @@
-import { BaseRepository } from '@/services/base'
-import request from '@/services/configs/axios.config'
-
-export default class AuthRepository extends BaseRepository {
-  constructor() {
-    super('api/v1/auth')
-  }
-}
